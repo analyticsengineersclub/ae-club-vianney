029570d9e8e9000b48018b24edf7e9bd2aa68201
Vianney Martinez <68610712+vamartinezi@users.noreply.github.com>
2022-10-07 15:43:07 -0500

    Update session_num.sql
    cleaned up

--- session_num.sql
+++ session_num.sql
@@ -1,18 +1,20 @@
 with base as (
-SELECT 
-visitor_id,
-timestamp as initial, 
-row_number() over (partition by visitor_id order by timestamp asc) as num,
-lag(timestamp) over (partition by visitor_id order by timestamp) as prev
-FROM `analytics-engineers-club.web_tracking.pageviews` 
---where visitor_id = '0f67a08b-1b78-4f30-ac39-15a1316390a0'
+select 
+  visitor_id,
+  timestamp as initial, 
+  row_number() over (partition by visitor_id order by timestamp asc) as num,
+  lag(timestamp) over (partition by visitor_id order by timestamp) as prev
+
+from `analytics-engineers-club.web_tracking.pageviews` 
 order by timestamp asc
 ),
-time_d as (
+
+time_differences as (
 select 
-visitor_id,
-initial, prev,
-timestamp_diff(initial,prev,second) as time_diff
+  visitor_id,
+  initial, 
+  prev,
+  timestamp_diff(initial,prev,second) as time_diff
 
 from base
 ),
@@ -26,8 +28,11 @@ session as (
 
 final as (
 select *,
-sum(is_new_session) over (partition by visitor_id order by initial rows between unbounded preceding and current row) as session_number 
-from session
+sum(is_new_session) over (partition by visitor_id order by initial rows 
+between unbounded preceding and current row) as session_number 
+
+  from session
 )
+
 select * from final
 order by visitor_id, initial
